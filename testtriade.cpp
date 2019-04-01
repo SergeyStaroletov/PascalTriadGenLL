2662ded892eacc4574399f1b2826c5e1a3918704
Sergey <serg_soft@mail.ru>
2019-04-01 20:35:32 +0700

    Tests beautify

--- testtriade.cpp
+++ testtriade.cpp
@@ -5,34 +5,6 @@
 #include "scaner.h"
 #include "semantic.h"
 
-bool TestTriade::runTest(QString &src, QString expected) {
-  QFile testFile("test.pas");
-
-  if (testFile.open(QIODevice::WriteOnly)) {
-    QTextStream out(&testFile);
-    out << src;
-    testFile.close();
-  }
-
-  getdata("test.pas");
-
-  if (syntax() != 0) return false;
-
-  QFile resultFile("triada.ёклмн");
-  QString testResult;
-
-  if (resultFile.open(QIODevice::ReadOnly)) {
-    QTextStream in(&resultFile);
-    testResult = in.readAll();
-    testFile.close();
-  }
-
-  qDebug() << testResult;
-  qDebug() << expected;
-
-  return testResult.remove('\n') == expected.remove('\n');
-}
-
 void TestTriade::testTrivial() {
   // QSKIP("hi");
   QString src =
@@ -76,7 +48,7 @@ void TestTriade::testFunction() {
       "   a:= f(12356 div 123);"
       "  end.";
   QString expected =
-      "1) proc f 1"
+      "1) proc f 1"  // one local var
       "2) + i 1"
       "3) := f (2)"
       "4) endp f 1"
@@ -100,18 +72,47 @@ void TestTriade::testWhile() {
               end;\
           end.";
   QString expected =
-      "1) := a 0\n"
-      "2) metka _m0 \n"
-      "3) < a 10\n"
-      "4) cmp (3) \n"
-      "5) jfalse _m1 \n"
-      "6) := b 5\n"
-      "7) - a 1\n"
-      "8) := a (7)\n"
-      "9) go _m0 \n"
-      "10) metka _m1 \n";
+      "1) := a 0"
+      "2) metka _m0 "
+      "3) < a 10"
+      "4) cmp (3) "
+      "5) jfalse _m1 "
+      "6) := b 5"
+      "7) - a 1"
+      "8) := a (7)"
+      "9) go _m0 "
+      "10) metka _m1 ";
 
   QVERIFY(runTest(src, expected));
+}
+
+// magic
+bool TestTriade::runTest(QString &src, QString expected) {
+  QFile testFile("test.pas");
+
+  if (testFile.open(QIODevice::WriteOnly)) {
+    QTextStream out(&testFile);
+    out << src;
+    testFile.close();
+  }
+
+  getdata("test.pas");
+
+  if (syntax() != 0) return false;
+
+  QFile resultFile("triada.ёклмн");
+  QString testResult;
+
+  if (resultFile.open(QIODevice::ReadOnly)) {
+    QTextStream in(&resultFile);
+    testResult = in.readAll();
+    testFile.close();
+  }
+
+  qDebug() << testResult;
+  qDebug() << expected;
+
+  return testResult.remove('\n') == expected.remove('\n');
 }
 
 QTEST_MAIN(TestTriade)
